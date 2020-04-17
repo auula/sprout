11f3e04d275d147f5f8201309e1161eceb4ddbfe
Dings <deen.job@qq.com>
2020-04-17 22:37:52 +0800

    ❎repair logging level to string format.🚀 v1.1.6

--- caller.go
+++ caller.go
@@ -26,5 +26,6 @@ func buildCallerStr(skip int) string {
 	// file path
 	filePath := path.Base(file)
 	// build string runtime caller info
-	return fmt.Sprintf("- Pc:%d - Position: %s|%s:%d", pc, filePath, funName, line)
+	//return fmt.Sprintf("- Pc:%d - Position: %s|%s:%d", pc, filePath, funName, line)
+	return fmt.Sprintf("- Position: %s|%s:%d", filePath, funName, line)
 }
