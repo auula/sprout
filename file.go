e8f8fea18b20d3d4925f015732ae230c8692fcb8
Dings <deen.job@qq.com>
2020-04-17 19:02:05 +0800

    ❎repair logging level not out problem.🚀 v1.1.6

--- file.go
+++ file.go
@@ -5,14 +5,34 @@
 
 package logker
 
-import "os"
+import (
+	"errors"
+	"os"
+	"path"
+)
+
+//Customize system power
+type Power os.FileMode
 
 type fileLog struct {
-	WheError  bool     // whether enable error log file.
-	Directory string   // Log save directory
-	FileName  string   // Log file name
-	File      *os.File // Log file  pointer
-	ErrFile   *os.File // Error file log pointer
+	WheError  bool        // Whether enable error log file.
+	Directory string      // Log save directory
+	FileName  string      // Log file name
+	File      *os.File    // Log file  pointer
+	ErrFile   *os.File    // Error file log pointer
+	tz        *timeZone   // Customize of time zone type
+	timeZone  logTimeZone // Set running Time Zone
+	Pw        Power       // File Power
+}
+
+// Initialization error file pointer
+func (f *fileLog) initErrPtr() (*os.File, error) {
+	savePath := path.Join(f.Directory, "error_", f.FileName, ".log")
+	file, e := os.OpenFile(savePath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, f.Pw)
+	if e == nil {
+		return nil, errors.New("open file fail :" + savePath)
+	}
+	return file, nil
 }
 
 func (f *fileLog) isEnableErr() bool {
