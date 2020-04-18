c1587a03518ab3408d1315e385eb4a079a57ad70
Dings <deen.job@qq.com>
2020-04-18 20:43:03 +0800

    push v1.0.8🚀

---
+++ console.go
@@ -0,0 +1,56 @@
+// Copyright (c) 2020 HigKer
+// Open Source: MIT License
+// Author: SDing <deen.job@qq.com>
+// Date: 2020/4/16 - 9:36 下午
+
+package logker
+
+import "fmt"
+
+// console Logger
+type console struct {
+	// Logging level
+	logLevel level
+	// set running Time Zone
+	timeZone logTimeZone
+	// customize of time zone type
+	tz *timeZone
+	// Whether enable console color
+	// code : WheColor bool
+}
+
+func (c *console) initTime() {
+	// set customize time zone
+	c.tz = &timeZone{TimeZoneStr: c.timeZone}
+}
+
+// TODO: Whether enable current level
+func (c *console) isEnableLevel(lev level) bool {
+	// debug<info<warn<Error
+	return c.logLevel <= lev
+}
+
+//func (c *console) IsEnableColor() bool {
+//	return c.WheColor
+//}
+
+func (c *console) Info(value string, args ...interface{}) {
+	if c.isEnableLevel(INFO) {
+		c.outPutMessage(INFO, fmt.Sprintf(value, args...))
+	}
+}
+func (c *console) Debug(value string, args ...interface{}) {
+	if c.isEnableLevel(DEBUG) {
+		c.outPutMessage(DEBUG, fmt.Sprintf(value, args...))
+	}
+}
+func (c *console) Error(value string, args ...interface{}) {
+	if c.isEnableLevel(ERROR) {
+		c.outPutMessage(ERROR, fmt.Sprintf(value, args...))
+	}
+}
+func (c *console) Warning(value string, args ...interface{}) {
+	if c.isEnableLevel(WARNING) {
+		c.outPutMessage(WARNING, fmt.Sprintf(value, args...))
+	}
+}
